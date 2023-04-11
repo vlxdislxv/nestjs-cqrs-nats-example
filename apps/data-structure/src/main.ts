39d65b25d6febbd7a87eb037296c511752b1a584
vlxdislxv <skyvvader@gmail.com>
2023-04-11 11:56:11 +0300

    Base repo setup.

---
+++ apps/data-structure/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.NATS,
+    options: {
+      servers: ['nats://localhost:4222'],
+    },
+  });
+
+  await app.listen();
+}
+bootstrap();
